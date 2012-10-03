dbdf9d7e832674a93e46048b0d2df328ca8220ed
leszek.porebski@gmail.com <leszek.porebski@gmail.com@afeb9a10-b954-e640-7480-283e2bab1b68>
2012-10-03 17:12:26 +0000

    [ReadingAssistant] Initial commit [ReadingAssistant] Basic layout

---
+++ trunk/App/ReadingAssistant/ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReadingAssistant
+{
+    class ViewModel: DependencyObject
+    {
+        #region HighlightingSpeed dependancy property
+        // Using a DependencyProperty as the backing store for HighlightingSpeed.
+        public static readonly DependencyProperty HighlightingSpeedProperty =
+            DependencyProperty.Register("HighlightingSpeed", typeof(double), typeof(ViewModel));
+
+        public double HighlightingSpeed
+        {
+            get { return (double)GetValue(HighlightingSpeedProperty); }
+            set { SetValue(HighlightingSpeedProperty, value); }
+        }
+        #endregion
+
+        #region InputTextFilePath dependancy property
+        // Using a DependencyProperty as the backing store for InputTextFilePath.
+        public static readonly DependencyProperty InputTextFilePathProperty =
+            DependencyProperty.Register("InputTextFilePath", typeof(string), typeof(ViewModel));
+
+        public string InputTextFilePath
+        {
+            get { return (string)GetValue(InputTextFilePathProperty); }
+            set { SetValue(InputTextFilePathProperty, value); }
+        }
+        #endregion
+
+    }
+}
