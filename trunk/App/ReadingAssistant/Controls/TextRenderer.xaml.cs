9d77e5cbdd00ea7e925d3b242cd92cc315eaf6f9
leszek.porebski@gmail.com <leszek.porebski@gmail.com@afeb9a10-b954-e640-7480-283e2bab1b68>
2012-10-12 23:41:11 +0000

    [ReadingAssistant] Ok... so we know how to render formatted text ;)

--- trunk/App/ReadingAssistant/Controls/TextRenderer.xaml.cs
+++ trunk/App/ReadingAssistant/Controls/TextRenderer.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,7 @@ namespace ReadingAssistant.Controls
     /// <summary>
     /// Interaction logic for TextRenderer.xaml
     /// </summary>
-    public partial class TextRenderer : TextBlock
+    public partial class TextRenderer : Control
     {
         public TextRenderer()
         {
@@ -36,5 +37,51 @@ namespace ReadingAssistant.Controls
             set { SetValue(ViewModelProperty, value); }
         }
         #endregion
+
+        #region Text dependancy property
+        // Using a DependencyProperty as the backing store for Text.
+        public static readonly DependencyProperty TextProperty =
+            TextBlock.TextProperty.AddOwner(typeof(TextRenderer), 
+                new FrameworkPropertyMetadata(string.Empty, 
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    OnTextChanged));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private static void OnTextChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            ((TextRenderer)sender).createFormattedTextFromString(args.NewValue as string);
+        }
+        #endregion
+
+        #region TextToRender property
+        private FormattedText _textToRender;
+
+        private void createFormattedTextFromString(string text)
+        {
+            _textToRender = new FormattedText(
+                text,
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface("Verdana"),
+                32,
+                Brushes.Black);
+        }
+        #endregion
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            if (_textToRender != null)
+            {
+                _textToRender.MaxTextWidth = this.ActualWidth;
+                drawingContext.DrawText(_textToRender, new Point());
+            }
+        }
     }
 }
